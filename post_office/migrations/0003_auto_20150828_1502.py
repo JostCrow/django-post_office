b3e68e64dcf1d2ae8a9f7b991176e2f01b2c7f34
Jorik Kraaikamp <jorik.kraaikamp@maykinmedia.nl>
2016-07-01 17:57:39 +0200

    Fixed the migrations path. Added the merge migration

--- post_office/migrations/0003_auto_20150828_1502.py
+++ post_office/migrations/0003_auto_20150828_1502.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('post_office', '0003_longer_subject'),
+        ('post_office', '0002_add_i18n_and_backend_alias'),
     ]
 
     operations = [
---
+++ post_office/migrations/0004_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('post_office', '0003_longer_subject'),
+        ('post_office', '0003_auto_20150828_1502'),
+    ]
+
+    operations = [
+    ]
